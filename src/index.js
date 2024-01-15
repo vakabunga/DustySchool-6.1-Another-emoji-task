f8e1b94b88cd9ee1e708c4ff222e427632a34538
vakabunga <shesterov@gmail.com>
2024-01-16 01:22:17 +0300

    create form where u can type message, pick emoji and send in to the next container aka chat window

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable new-cap */
+/* eslint-disable no-new */
+new lc_emoji_picker('textarea, input');
+
+const chatWindow = document.querySelector('.chat-window');
+const form = document.querySelector('.form');
+const input = document.querySelector('.input');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const message = document.createElement('p');
+  message.classList.add('message');
+  message.textContent = input.value;
+  chatWindow.appendChild(message);
+  input.value = '';
+});
